59f3790823f872a884e559e9b12f638dffd0a55c
MaxineHer <maxine.herrera@gmail.com>
2024-10-06 17:00:26 +0400

    first commit

---
+++ st.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+st.write("COSMIC SWIMMERS WEATHER DASHBOARD")
+
+df = pd.read_csv("C:\\Users\\maxin\\OneDrive\\Documents\\School\\hackthonnasa\\avgs.csv")
+choices = ["temperature", "precipitation","windspeed"]
+cat = st.selectbox("Choose Primary Type: ", choices, index=0)
+
+if (cat == "temperature"):
+    fig = px.choropleth(data_frame=df, locationmode="country names", scope="world", locations="Country", color="temperature", title="Temperatures")
+if (cat == "precipitation"):
+    fig = px.choropleth(data_frame=df, locationmode="country names", scope="world", locations="Country", color="precipitation", title="Precipitations")
+if (cat == "windspeed"):
+    fig = px.choropleth(data_frame=df, locationmode="country names", scope="world", locations="Country", color="windspeed", title="Wind Speeds")
+
+st.plotly_chart(fig, use_container_width=True)
